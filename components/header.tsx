07c348cbd617159e3672cae93e3e8abe46b7a029
JhoncSilva <jhoncs.004@gmail.com>
2020-11-28 19:58:03 -0300

    links header

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="border-b w-full border-white p-2 flex justify-around">
-        <a href=""><Image src="/insta.png" height={30} width={30}/></a>
-        <a href=""><Image src="/github.png" height={30} width={30}/></a>
+        <a target="_blank" href="https://www.instagram.com/davilima_designer/"><Image src="/insta.png" height={30} width={30}/></a>
+        <a target="_blank" href="https://github.com/JhoncSilva"><Image src="/github.png" height={30} width={30}/></a>
     </header>
   );
 }
